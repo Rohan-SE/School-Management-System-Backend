ad5ff66b6d132de9959752da365994dc14115d00
Rohan Ahmad <rohanse.1.ra@gmail.com>
2025-07-01 00:21:32 +0500

    adminSchema Created

---
+++ models/adminModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const adminSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password:{
+        type:String,
+        required:true,
+        min:[6,"Password must be 6 length"],
+        max:10
+    }
+})
+
+const adminModel = mongoose.model('admin',adminSchema)
+
+export default adminModel
